acbb33900797ec3f0f299f3ecb52a8fa0b0b0a76
jandarahasia <85703474+jandarahasia@users.noreply.github.com>
2021-12-02 20:59:25 +0700

    Update join.js

--- script/join.js
+++ script/join.js
@@ -51,10 +51,10 @@ function Share() {
         document.getElementById("kunna").innerHTML = "99%";
     }
     if (clicks == 11) {
-        setTimeout("window.location.href = 'https://bit.ly/adultmovieh';", 0);
+        location.href = 'https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=whatsapp';
     }
 };
 
 function Link() {
-window.location.href = 'https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=whatsapp';
+location.href = 'https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=whatsapp';
 }
